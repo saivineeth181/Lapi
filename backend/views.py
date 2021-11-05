1680ef172749a3f9d2bb60b4e317b4904e5b9806
saivineeth181 <saivineeth181@gmail.com>
2021-11-05 11:40:37 +0530

    'list'

--- backend/views.py
+++ backend/views.py
@@ -1,8 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import inventory,Patrons
-from .serializers import inventorySerializer,PatronsSerializer
-from datetime import datetime
+from .models import inventory,Patrons,List
+from .serializers import inventorySerializer,PatronsSerializer,ListSerializer
 import requests
 
 
@@ -32,16 +31,13 @@ def Inventory_cam(request):
     data_json = {
         "title":title,
         "book_over_pic":book_cover_pic,
-        "quantity":quantity
-
+        "quantity":quantity,
     }
     serializer = inventorySerializer(data=data_json)
     if(serializer.is_valid()):
         serializer.save()
     
     return Response(serializer.data)
-
-    
 
 @api_view(["POST"])
 def Inventory_create(request):
@@ -74,8 +70,6 @@ def Patrons_view(request):
     
     return Response(serializer.data)
 
-  
-
 @api_view(["POST"])
 def Patrons_create(request):
     serializer = PatronsSerializer(data=request.data)
@@ -96,6 +90,40 @@ def Patrons_update(request,pk):
 @api_view(["DELETE"])
 def Patrons_delete(request,pk):
     task = Patrons.objects.get(id=pk)
+    task.delete()
+    
+    return Response('deleted!')
+
+@api_view(['GET'])
+def List_view(request,username):
+    
+    User = Patrons.objects.get(name=username)
+    task = List.objects.filter(name=int(User.id))
+    serializer = ListSerializer(task,many=True)
+    
+    return Response(serializer.data)
+
+@api_view(["POST"])
+def List_create(request):
+    
+    serializer = ListSerializer(data=request.data)
+    if(serializer.is_valid()):
+        serializer.save()
+    
+    return Response(serializer.data)
+
+@api_view(["PUT"])
+def List_update(request,pk):
+    task = List.objects.get(id=pk)
+    serializer = ListSerializer(instance=task, data=request.data)
+    if(serializer.is_valid()):
+        serializer.save()
+    
+    return Response(serializer.data)
+    
+@api_view(["DELETE"])
+def List_delete(request,pk):
+    task = List.objects.get(id=pk)
     task.delete()
     
     return Response('deleted!')
